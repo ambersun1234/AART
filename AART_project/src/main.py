652f441301c0bcc6325432a9779ce877db7e49d6
ambersun <ambersun1019.shawn@gmail.com>
2019-03-29 01:31:11 +0800

    Add 4 panel to main

--- AART_project/src/main.py
+++ AART_project/src/main.py
@@ -4,128 +4,87 @@ import wx.media
 import cv2
 
 from src.device.device import SelectDeviceDialog
-
-class StaticText(wx.StaticText):
-	"""
-	A StaticText that only updates the label if it has changed, to
-	help reduce potential flicker since these controls would be
-	updated very frequently otherwise.
-	"""
-
-	def SetLabel(self, label):
-		if label != self.GetLabel():
-			wx.StaticText.SetLabel(self, label)
-
-class Panel(wx.Panel):
-	def __init__(self, parent):
-		wx.Panel.__init__(self, parent, -1, style=wx.TAB_TRAVERSAL | wx.CLIP_CHILDREN)
-		try:
-			self.mc = wx.media.MediaCtrl(self, style=wx.SIMPLE_BORDER)
-		except NotImplementedError:
-			self.Destroy()
-			raise
-
-		self.Bind(wx.media.EVT_MEDIA_LOADED, self.OnMediaLoaded)
-
-		btn2 = wx.Button(self, -1, "Play")
-		self.Bind(wx.EVT_BUTTON, self.OnPlay, btn2)
-		self.playBtn = btn2
-
-		btn3 = wx.Button(self, -1, "Pause")
-		self.Bind(wx.EVT_BUTTON, self.OnPause, btn3)
-
-		btn4 = wx.Button(self, -1, "Stop")
-		self.Bind(wx.EVT_BUTTON, self.OnStop, btn4)
-
-		slider = wx.Slider(self, -1, 0, 0, 0)
-		self.slider = slider
-		self.st_size = StaticText(self, -1, size=(100, -1))
-		self.st_len = StaticText(self, -1, size=(100, -1))
-		self.st_pos = StaticText(self, -1, size=(100, -1))
-
-		sizer = wx.GridBagSizer(5, 5)
-		sizer.Add(self.mc, (1, 1), span=(5, 1))  # , flag=wx.EXPAND)
-		sizer.Add(btn2, (2, 3))
-		sizer.Add(btn3, (3, 3))
-		sizer.Add(btn4, (4, 3))
-		sizer.Add(slider, (6, 1), flag=wx.EXPAND)
-		sizer.Add(self.st_size, (1, 5))
-		sizer.Add(self.st_len, (2, 5))
-		sizer.Add(self.st_pos, (3, 5))
-		self.SetSizer(sizer)
-
-		self.timer = wx.Timer(self)
-		self.Bind(wx.EVT_TIMER, self.OnTimer)
-		self.timer.Start(100)
-
-	def DoLoadFile(self, path):
-		self.playBtn.Disable()
-
-		if not self.mc.Load(path):
-			wx.MessageBox("Unable to load %s: Unsupported format?" % path,
-                            "ERROR",
-                            wx.ICON_ERROR | wx.OK)
-		else:
-			self.mc.SetInitialSize()
-			self.GetSizer().Layout()
-			self.slider.SetRange(0, self.mc.Length())
-
-	def OnMediaLoaded(self, evt):
-		self.playBtn.Enable()
-
-	def OnPlay(self, evt):
-		if not self.mc.Play():
-			wx.MessageBox("Unable to Play media : Unsupported format?",
-                            "ERROR",
-                            wx.ICON_ERROR | wx.OK)
-		else:
-			self.mc.SetInitialSize()
-			self.GetSizer().Layout()
-			self.slider.SetRange(0, self.mc.Length())
-
-	def OnPause(self, evt):
-		self.mc.Pause()
-
-	def OnStop(self, evt):
-		self.mc.Stop()
-
-	def OnSeek(self, evt):
-		offset = self.slider.GetValue()
-		self.mc.Seek(offset)
-
-	def OnTimer(self, evt):
-		offset = self.mc.Tell()
-		self.slider.SetValue(offset)
-		self.st_size.SetLabel('size: %s' % self.mc.GetBestSize())
-		self.st_len.SetLabel('length: %d seconds' % (self.mc.Length() / 1000))
-		self.st_pos.SetLabel('position: %d' % offset)
-
-	def ShutdownDemo(self):
-		self.timer.Stop()
-		del self.timer
-
+from src.input.input import InputPanel
+from src.media.media import MediaPanel
+from src.output.output import outputTextPanel, outputPicPanel
 
 class Frame(wx.Frame):
 	def __init__(self, parent, title):
 		super(Frame, self).__init__(parent, title=title)
+		self.currentScreenX = 0
+		self.currentScreenY = 0
+		self.initSize()
+
+		self.mediaPanel = MediaPanel(
+			self,
+			size=(
+				self.currentScreenX * 0.7,
+				self.currentScreenY * 0.7
+			)
+		)
+		self.outputPicPanel = outputPicPanel(
+			self,
+			size=(
+				self.currentScreenX * 0.7,
+				self.currentScreenY * 0.3
+			)
+		)
+
+		self.inputPanel = InputPanel(
+			self,
+			size=(
+				self.currentScreenX * 0.3,
+				self.currentScreenY * 0.3
+			)
+		)
+		self.outputTextPanel = outputTextPanel(
+			self,
+			size=(
+				self.currentScreenX * 0.3,
+				self.currentScreenY * 0.7
+			)
+		)
+
 		self.initUI()
 
 	def initUI(self):
 		self.initMenuBar()
-		self.initSize()
+
+		vbox = wx.BoxSizer(wx.VERTICAL)
+		hboxUp = wx.BoxSizer(wx.HORIZONTAL)
+		hboxBottom = wx.BoxSizer(wx.HORIZONTAL)
+
+		hboxUp.Add(self.mediaPanel)
+		hboxUp.AddSpacer(5)  # add 5px space
+		hboxUp.Add(self.outputTextPanel)
+
+		hboxBottom.Add(self.outputPicPanel)
+		hboxBottom.AddSpacer(5)  # add 5px space
+		hboxBottom.Add(self.inputPanel)
+
+		vbox.Add(hboxUp)
+		vbox.AddSpacer(5)
+		vbox.Add(hboxBottom)
+
+		self.SetSizer(vbox)
 		self.Show()
 
 	def initSize(self):
 		screenX, screenY = wx.GetDisplaySize()
-		screenX = screenX * 0.75
-		screenY = screenY * 0.8
+		self.currentScreenX = screenX * 0.75
+		self.currentScreenY = screenY * 0.8
 
-		self.SetSize(screenX, screenY)
+		self.SetSize(self.currentScreenX, self.currentScreenY)
 		self.Centre()
 
 	def initMenuBar(self):
 		menuBar = wx.MenuBar()
-		menuBar.SetFont(wx.Font(13, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
+		menuBar.SetFont(wx.Font(
+			13,
+			wx.FONTFAMILY_DEFAULT,
+			wx.FONTSTYLE_NORMAL,
+			wx.FONTWEIGHT_NORMAL)
+		)
 		fileMenu = wx.Menu()
 
 		op = wx.Menu()
@@ -147,11 +106,17 @@ class Frame(wx.Frame):
 		dialog.Destroy()
 
 	def onOpenVideo(self, event):
-		dialog = wx.FileDialog(self, message="Choose a file", wildcard="*.mp4", defaultFile="", style=wx.FD_OPEN | wx.FD_MULTIPLE | wx.FD_CHANGE_DIR)
+		dialog = wx.FileDialog(
+			self,
+			message="Choose a file",
+			wildcard="*.mp4",
+			defaultFile="",
+			style=wx.FD_OPEN | wx.FD_CHANGE_DIR
+		)
 		if dialog.ShowModal() == wx.ID_OK:
 			paths = dialog.GetPaths()
 			for path in paths:
-				Panel.DoLoadFile(panel, path)
+				MediaPanel.DoLoadFile(self.mediaPanel, path)
 		dialog.Destroy()
 
 	def OnQuit(self, event):
@@ -159,6 +124,8 @@ class Frame(wx.Frame):
 
 if __name__ == '__main__':
 	app = wx.App()
-	frame = Frame(None, title="Athlete Analysis of Real Time Sports Events( AART )")
-	panel = Panel(frame)
+	frame = Frame(
+		None,
+		title="Athlete Analysis of Real Time Sports Events( AART )"
+	)
 	app.MainLoop()
