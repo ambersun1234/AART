33ccadb3ca4ef5a19954c7460c9836d68889cc43
ambersun <ambersun1019.shawn@gmail.com>
2019-03-29 17:11:08 +0800

    Fix multi same web camera

--- AART_project/src/device/device.py
+++ AART_project/src/device/device.py
@@ -5,6 +5,7 @@ class SelectDeviceDialog(wx.Dialog):
 	def __init__(self, *args, **kwargs):
 		super(SelectDeviceDialog, self).__init__(*args, **kwargs)
 
+		self.deviceID_t = -1
 		self.deviceID = -1
 		self.device = DeviceCheck.deviceQuery()
 		self.InitUI()
@@ -13,11 +14,12 @@ class SelectDeviceDialog(wx.Dialog):
 		pnl = wx.Panel(self)
 		vbox = wx.BoxSizer(wx.VERTICAL)
 
-		sb = wx.StaticBox(pnl, label='Colors')
+		sb = wx.StaticBox(pnl, label='Select web cameras')
 		sbs = wx.StaticBoxSizer(sb, orient=wx.VERTICAL)
 
 		for key, value in self.device.items():
-			sbs.Add(wx.RadioButton(pnl, label=key))
+			temp = wx.RadioButton(pnl, label=key)
+			sbs.Add(temp)
 
 		hbox = wx.BoxSizer(wx.HORIZONTAL)
 		okButton = wx.Button(self, label='Ok')
@@ -25,54 +27,108 @@ class SelectDeviceDialog(wx.Dialog):
 		hbox.Add(okButton)
 		hbox.Add(closeButton, flag=wx.LEFT, border=5)
 
-		vbox.Add(pnl, proportion=1,
-                    flag=wx.ALL | wx.EXPAND, border=5)
-		vbox.Add(hbox, flag=wx.ALIGN_CENTER | wx.TOP | wx.BOTTOM, border=10)
+		vbox.Add(
+			pnl,
+			proportion=1,
+			flag=wx.ALL | wx.EXPAND,
+			border=5
+		)
+		vbox.Add(hbox, flag=wx.ALIGN_CENTER | wx.TOP | wx.BOTTOM)
 
+		pnl.SetSizer(sbs)
 		self.SetSizer(vbox)
 		self.Bind(wx.EVT_RADIOBUTTON, self.onSelect)
 
-		okButton.Bind(wx.EVT_RADIOBUTTON, self.onClose)
+		okButton.Bind(wx.EVT_RADIOBUTTON, self.onOk)
 		closeButton.Bind(wx.EVT_BUTTON, self.onClose)
 
 	def onSelect(self, event):
-		self.deviceID = int(self.device[event.GetEventObject().GetLabel()])
-		print(self.deviceID)
+		self.deviceID_t = int(self.device[event.GetEventObject().GetLabel()])
+		print(self.deviceID_t)
+
+	def onOk(self):
+		self.deviceID = self.deviceID_t
 
 	def onClose(self, event):
 		self.Destroy()
 
 class DeviceCheck:
 	@staticmethod
+	def getId(idt):
+		ids = dict()
+		repeatMapping = dict()
+
+		for element in idt.split("|"):
+			id, index = element.split("_")
+
+			if id == " ." or id == " ..":
+				continue
+
+			if ":" not in str(id):
+				ids[id] = index
+			else:
+				first = str(id).split(":")[0]
+				second = str(id).split(":")[1]
+				repeat = str(id).split(":")[2]
+
+				currentId = "{}:{}".format(first, second)
+
+				repeatMapping[currentId] = repeatMapping.get(currentId, 0) + 1
+				ids["{}:{}".format(currentId, repeat)] = index
+
+		# construct mapping count
+		repeatMappingCount = {x: 0 for x in repeatMapping.keys()}
+		return ids, repeatMapping, repeatMappingCount
+
+	@staticmethod
+	def getUsb(usbt):
+		usbs = dict()
+
+		for element in usbt.split("|"):
+			id, name = element.split(" ", 1)
+			usbs[id] = name
+		return usbs
+
+	@staticmethod
 	def deviceQuery():
 		subprocess.call(["chmod +x ./fetch.sh"], shell=True)
-		p = subprocess.Popen("./fetch.sh", shell=True, stdout=subprocess.PIPE, executable="/bin/bash")
+		p = subprocess.Popen(
+			"./fetch.sh",
+			shell=True,
+			stdout=subprocess.PIPE,
+			executable="/bin/bash"
+		)
 		output, err = p.communicate()
 		errorcode = p.returncode
 
 		if errorcode == 0:
 			output = str(output)[2:-1]
 			idt, usbt = output.split("@")
-			ids = dict()
-			usbs = dict()
+			ids, repeatMapping, repeatMappingCount = DeviceCheck.getId(idt)
+			usbs = DeviceCheck.getUsb(usbt)
 			devices = dict()
-
-			# fetch id
-			for element in idt.split("|"):
-				id, index = element.split("_")
-				ids[id] = index
-			for element in usbt.split("|"):
-				id, name = element.split(" ", 1)
-				usbs[id] = name
 
 			# id name mapping
 			for id, index in ids.items():
-				if id in usbs:
-					devices[usbs[id]] = index
-			diff = set(ids.values()) - set(devices.values())
+				if ":" in id:
+					twoId = "{}:{}".format(str(id).split(":")[0], str(id).split(":")[1])
 
+					if twoId in usbs:
+						if repeatMapping[twoId] <= 1:
+							devices["{}".format(usbs[twoId])] = index
+						else:
+							devices["{} [ {} ]".format(
+								usbs[twoId],
+								repeatMappingCount[twoId] + 1
+							)] = index
+						repeatMappingCount[twoId] += 1
+				else:
+					if id in usbs:
+						devices[usbs[id]] = index
+
+			diff = set(ids.values()) - set(devices.values())
 			if len(diff) != 0:
 				id = diff.pop()
 				devices["Build in web camera"] = id
 
-		return devices
+		return {k: v for k, v in sorted(devices.items(), key=lambda x: x[1])}
--- AART_project/src/fetch.sh
+++ AART_project/src/fetch.sh
@@ -2,15 +2,16 @@ IFS=$'\n'
 id=()
 usb=($(lsusb | awk -F "ID " '{print $2}'))
 byid=($(ls -al /dev/v4l/by-id | cut -c42-))
+
 for i in "${byid[@]}"
 	do
 		if [[ "${i}" = "." ]] || [[ "${i}" = ".." ]]; then
 			continue
 		fi
 		first=$(echo ${i} | awk -F ' ->' '{print $1}')
-		temp=$(echo ${first} | cut -d "-" -f2- | cut -d "_" -f-2 | tr "_" ":")
-		temp2=$(echo ${first} | rev | cut -d "-" -f1 | rev | sed 's/[a-z]//g')
-		id+="${temp}_${temp2}"
+		temp=$(echo ${first} | cut -d "-" -f2- | cut -d "-" -f-1 | tr "_" ":")
+		temp2=$(echo ${i} | awk -F ' ->' '{print $2}' | cut -c13-)
+		id=("${id[@]}" "${temp}_${temp2}")
 	done
 
 IFS='|'
